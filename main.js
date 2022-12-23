918844bcbe095304cf567f17a074f081ff10abea
vosid <temirboyev0823@gmail.com>
2022-12-23 19:21:22 +0500

    show personal best

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const divs = document.querySelectorAll('.con'),
 	msg = document.querySelector('#result'),
-	span = document.querySelector('span')
+	span = document.querySelector('span'),
+	pb = document.querySelector('.best')
 
 let timeout
 let startTime
 let endTime
+let results = []
 
 divs[0].addEventListener('click', () => {
 	let time = (Math.random() * 3 + 2).toFixed(0)
@@ -15,7 +17,7 @@ divs[0].addEventListener('click', () => {
 		divs[1].classList.remove('active')
 		divs[2].classList.add('active')
 		startTime = performance.now()
-	}, time * 1000)
+	}, time * 100)
 })
 
 divs[1].addEventListener('click', () => {
@@ -28,8 +30,12 @@ divs[1].addEventListener('click', () => {
 
 divs[2].addEventListener('click', () => {
 	endTime = performance.now()
+	let last = (endTime - startTime).toFixed(0)
 	divs[2].classList.remove('active')
 	divs[0].classList.add('active')
-	msg.textContent = `${(endTime - startTime).toFixed(0)} ms`
+	msg.textContent = `${last} ms`
 	span.textContent = 'click to continue'
+	
+	results.push(last)
+	pb.textContent = `personal best: ${Math.min(...results)} ms`
 })
